95ce43507e1740a33d8f040e07d340ad1a47706c
Lucas Werner <lucaswerner26@gmail.com>
2020-03-31 01:50:39 -0300

    first commit:

---
+++ src/test/java/com/mycard/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycard.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
